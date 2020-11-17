9c6bdfc5070870e758b92d948f4ac60dac3789b7
nmeeshell <65826269+nmeeshell@users.noreply.github.com>
2020-11-16 21:20:13 -0600

    mission to mars

--- .ipynb_checkpoints/mission_to_mars-checkpoint.py
+++ .ipynb_checkpoints/mission_to_mars-checkpoint.py
@@ -2,228 +2,309 @@
  "cells": [
   {
    "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "from bs4 import BeautifulSoup as bs\n",
+    "from splinter import Browser\n",
+    "import requests\n",
+    "import pandas as pd"
+   ]
+  },
+  {
+   "cell_type": "code",
    "execution_count": 2,
    "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "The history saving thread hit an unexpected error (OperationalError('database or disk is full')).History will not be written to the database.\n"
-     ]
-    },
-    {
-     "ename": "ModuleNotFoundError",
-     "evalue": "No module named 'splinter'",
-     "output_type": "error",
-     "traceback": [
-      "\u001b[0;31m---------------------------------------------------------------------------\u001b[0m",
-      "\u001b[0;31mModuleNotFoundError\u001b[0m                       Traceback (most recent call last)",
-      "\u001b[0;32m<ipython-input-2-f93e1212c03a>\u001b[0m in \u001b[0;36m<module>\u001b[0;34m\u001b[0m\n\u001b[1;32m      1\u001b[0m \u001b[0;31m# Declare Dependencies\u001b[0m\u001b[0;34m\u001b[0m\u001b[0;34m\u001b[0m\u001b[0;34m\u001b[0m\u001b[0m\n\u001b[1;32m      2\u001b[0m \u001b[0;32mfrom\u001b[0m \u001b[0mbs4\u001b[0m \u001b[0;32mimport\u001b[0m \u001b[0mBeautifulSoup\u001b[0m\u001b[0;34m\u001b[0m\u001b[0;34m\u001b[0m\u001b[0m\n\u001b[0;32m----> 3\u001b[0;31m \u001b[0;32mfrom\u001b[0m \u001b[0msplinter\u001b[0m \u001b[0;32mimport\u001b[0m \u001b[0mBrowser\u001b[0m\u001b[0;34m\u001b[0m\u001b[0;34m\u001b[0m\u001b[0m\n\u001b[0m\u001b[1;32m      4\u001b[0m \u001b[0;32mimport\u001b[0m \u001b[0mpandas\u001b[0m \u001b[0;32mas\u001b[0m \u001b[0mpd\u001b[0m\u001b[0;34m\u001b[0m\u001b[0;34m\u001b[0m\u001b[0m\n\u001b[1;32m      5\u001b[0m \u001b[0;32mimport\u001b[0m \u001b[0mrequests\u001b[0m\u001b[0;34m\u001b[0m\u001b[0;34m\u001b[0m\u001b[0m\n",
-      "\u001b[0;31mModuleNotFoundError\u001b[0m: No module named 'splinter'"
-     ]
-    }
-   ],
+   "outputs": [],
    "source": [
-    "# Declare Dependencies \n",
-    "from bs4 import BeautifulSoup\n",
-    "from splinter import Browser\n",
-    "import pandas as pd\n",
-    "import requests"
+    "!which chromedriver"
    ]
   },
   {
    "cell_type": "code",
-   "execution_count": null,
+   "execution_count": 3,
    "metadata": {},
    "outputs": [],
    "source": [
-    "executable_path = {'executable_path':'chromedriver.exe'}\n",
-    "browser = Browser('chrome', **executable_path, headless=False)"
+    "executable_path= {'executable_path': './chromedriver'}\n",
+    "browser = Browser('chrome', **executable_path)"
    ]
   },
   {
    "cell_type": "code",
-   "execution_count": null,
+   "execution_count": 4,
    "metadata": {},
    "outputs": [],
    "source": [
-    "# Visit Nasa news url through splinter module\n",
-    "url = 'https://mars.nasa.gov/news/'\n",
+    "url = \"https://mars.nasa.gov/news/\"\n",
     "browser.visit(url)"
    ]
   },
   {
    "cell_type": "code",
-   "execution_count": null,
+   "execution_count": 5,
    "metadata": {},
    "outputs": [],
    "source": [
-    "# HTML Object\n",
     "html = browser.html\n",
-    "\n",
-    "# Parse HTML with Beautiful Soup\n",
-    "soup = BeautifulSoup(html, 'html.parser')\n",
-    "\n",
-    "\n",
-    "# Retrieve the latest element that contains news title and news_paragraph\n",
-    "news_title = soup.find('div', class_='content_title').find('a').text\n",
-    "news_p = soup.find('div', class_='article_teaser_body').text\n",
-    "\n",
-    "# Display scrapped data \n",
-    "print(news_title)\n",
-    "print(news_p)"
+    "soup = bs(html, \"html.parser\")"
    ]
   },
   {
    "cell_type": "code",
-   "execution_count": null,
+   "execution_count": 6,
    "metadata": {},
-   "outputs": [],
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "'Mars Is Getting a New Robotic Meteorologist'"
+      ]
+     },
+     "execution_count": 6,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
    "source": [
-    "# Visit Mars Space Images through splinter module\n",
-    "image_url_featured = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'\n",
-    "browser.visit(image_url_featured)"
+    "news_title = soup.find('li',class_= 'slide')\n",
+    "news_title2 = news_title.find('div', class_='content_title').text\n",
+    "news_title2"
    ]
   },
   {
    "cell_type": "code",
-   "execution_count": null,
+   "execution_count": 7,
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "\"Sensors on NASA's Perseverance will help prepare for future human exploration by taking weather measurements and studying dust particles.\""
+      ]
+     },
+     "execution_count": 7,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "news_p = news_title.find('div', class_='article_teaser_body').text\n",
+    "news_p"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 8,
    "metadata": {},
    "outputs": [],
    "source": [
-    "# HTML Object \n",
-    "html_image = browser.html\n",
-    "\n",
-    "# Parse HTML with Beautiful Soup\n",
-    "soup = BeautifulSoup(html_image, 'html.parser')\n",
-    "\n",
-    "# Retrieve background-image url from style tag \n",
+    "url = \"https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars\"\n",
+    "browser.visit(url)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 9,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "html = browser.html\n",
+    "soup = bs(html, \"html.parser\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 10,
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "'https://www.jpl.nasa.gov/spaceimages/images/wallpaper/PIA19347-1920x1200.jpg'"
+      ]
+     },
+     "execution_count": 10,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
     "featured_image_url  = soup.find('article')['style'].replace('background-image: url(','').replace(');', '')[1:-1]\n",
-    "\n",
-    "# Website Url \n",
     "main_url = 'https://www.jpl.nasa.gov'\n",
-    "\n",
-    "# Concatenate website url with scrapped route\n",
     "featured_image_url = main_url + featured_image_url\n",
-    "\n",
-    "# Display full link to featured image\n",
     "featured_image_url"
    ]
   },
   {
    "cell_type": "code",
-   "execution_count": null,
+   "execution_count": 11,
    "metadata": {},
    "outputs": [],
    "source": [
-    "# Visit Mars Weather Twitter through splinter module\n",
-    "weather_url = 'https://twitter.com/marswxreport?lang=en'\n",
-    "browser.visit(weather_url)"
+    "mars_url = \"https://space-facts.com/mars/\"\n",
+    "mars_facts = pd.read_html(mars_url)"
    ]
   },
   {
    "cell_type": "code",
-   "execution_count": 1,
+   "execution_count": 12,
    "metadata": {},
    "outputs": [
     {
-     "ename": "NameError",
-     "evalue": "name 'browser' is not defined",
-     "output_type": "error",
-     "traceback": [
-      "\u001b[0;31m---------------------------------------------------------------------------\u001b[0m",
-      "\u001b[0;31mNameError\u001b[0m                                 Traceback (most recent call last)",
-      "\u001b[0;32m<ipython-input-1-97c1f2a058fa>\u001b[0m in \u001b[0;36m<module>\u001b[0;34m\u001b[0m\n\u001b[1;32m      1\u001b[0m \u001b[0;31m# HTML Object\u001b[0m\u001b[0;34m\u001b[0m\u001b[0;34m\u001b[0m\u001b[0;34m\u001b[0m\u001b[0m\n\u001b[0;32m----> 2\u001b[0;31m \u001b[0mhtml_weather\u001b[0m \u001b[0;34m=\u001b[0m \u001b[0mbrowser\u001b[0m\u001b[0;34m.\u001b[0m\u001b[0mhtml\u001b[0m\u001b[0;34m\u001b[0m\u001b[0;34m\u001b[0m\u001b[0m\n\u001b[0m\u001b[1;32m      3\u001b[0m \u001b[0;34m\u001b[0m\u001b[0m\n\u001b[1;32m      4\u001b[0m \u001b[0;31m# Parse HTML with Beautiful Soup\u001b[0m\u001b[0;34m\u001b[0m\u001b[0;34m\u001b[0m\u001b[0;34m\u001b[0m\u001b[0m\n\u001b[1;32m      5\u001b[0m \u001b[0msoup\u001b[0m \u001b[0;34m=\u001b[0m \u001b[0mBeautifulSoup\u001b[0m\u001b[0;34m(\u001b[0m\u001b[0mhtml_weather\u001b[0m\u001b[0;34m,\u001b[0m \u001b[0;34m'html.parser'\u001b[0m\u001b[0;34m)\u001b[0m\u001b[0;34m\u001b[0m\u001b[0;34m\u001b[0m\u001b[0m\n",
-      "\u001b[0;31mNameError\u001b[0m: name 'browser' is not defined"
-     ]
+     "data": {
+      "text/html": [
+       "<div>\n",
+       "<style scoped>\n",
+       "    .dataframe tbody tr th:only-of-type {\n",
+       "        vertical-align: middle;\n",
+       "    }\n",
+       "\n",
+       "    .dataframe tbody tr th {\n",
+       "        vertical-align: top;\n",
+       "    }\n",
+       "\n",
+       "    .dataframe thead th {\n",
+       "        text-align: right;\n",
+       "    }\n",
+       "</style>\n",
+       "<table border=\"1\" class=\"dataframe\">\n",
+       "  <thead>\n",
+       "    <tr style=\"text-align: right;\">\n",
+       "      <th></th>\n",
+       "      <th>Value</th>\n",
+       "    </tr>\n",
+       "    <tr>\n",
+       "      <th>Description</th>\n",
+       "      <th></th>\n",
+       "    </tr>\n",
+       "  </thead>\n",
+       "  <tbody>\n",
+       "    <tr>\n",
+       "      <th>Equatorial Diameter:</th>\n",
+       "      <td>6,792 km</td>\n",
+       "    </tr>\n",
+       "    <tr>\n",
+       "      <th>Polar Diameter:</th>\n",
+       "      <td>6,752 km</td>\n",
+       "    </tr>\n",
+       "    <tr>\n",
+       "      <th>Mass:</th>\n",
+       "      <td>6.39 × 10^23 kg (0.11 Earths)</td>\n",
+       "    </tr>\n",
+       "    <tr>\n",
+       "      <th>Moons:</th>\n",
+       "      <td>2 (Phobos &amp; Deimos)</td>\n",
+       "    </tr>\n",
+       "    <tr>\n",
+       "      <th>Orbit Distance:</th>\n",
+       "      <td>227,943,824 km (1.38 AU)</td>\n",
+       "    </tr>\n",
+       "    <tr>\n",
+       "      <th>Orbit Period:</th>\n",
+       "      <td>687 days (1.9 years)</td>\n",
+       "    </tr>\n",
+       "    <tr>\n",
+       "      <th>Surface Temperature:</th>\n",
+       "      <td>-87 to -5 °C</td>\n",
+       "    </tr>\n",
+       "    <tr>\n",
+       "      <th>First Record:</th>\n",
+       "      <td>2nd millennium BC</td>\n",
+       "    </tr>\n",
+       "    <tr>\n",
+       "      <th>Recorded By:</th>\n",
+       "      <td>Egyptian astronomers</td>\n",
+       "    </tr>\n",
+       "  </tbody>\n",
+       "</table>\n",
+       "</div>"
+      ],
+      "text/plain": [
+       "                                              Value\n",
+       "Description                                        \n",
+       "Equatorial Diameter:                       6,792 km\n",
+       "Polar Diameter:                            6,752 km\n",
+       "Mass:                 6.39 × 10^23 kg (0.11 Earths)\n",
+       "Moons:                          2 (Phobos & Deimos)\n",
+       "Orbit Distance:            227,943,824 km (1.38 AU)\n",
+       "Orbit Period:                  687 days (1.9 years)\n",
+       "Surface Temperature:                   -87 to -5 °C\n",
+       "First Record:                     2nd millennium BC\n",
+       "Recorded By:                   Egyptian astronomers"
+      ]
+     },
+     "execution_count": 12,
+     "metadata": {},
+     "output_type": "execute_result"
     }
    ],
    "source": [
-    "# HTML Object \n",
-    "html_weather = browser.html\n",
-    "\n",
-    "# Parse HTML with Beautiful Soup\n",
-    "soup = BeautifulSoup(html_weather, 'html.parser')\n",
-    "\n",
-    "# Find all elements that contain tweets\n",
-    "latest_tweets = soup.find_all('div', class_='js-tweet-text-container')\n",
-    "\n",
-    "# Retrieve all elements that contain news title in the specified range\n",
-    "# Look for entries that display weather related words to exclude non weather related tweets \n",
-    "for tweet in latest_tweets: \n",
-    "    weather_tweet = tweet.find('p').text\n",
-    "    if 'Sol' and 'pressure' in weather_tweet:\n",
-    "        print(weather_tweet)\n",
-    "        break\n",
-    "    else: \n",
-    "        pass"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "# Visit Mars facts url \n",
-    "facts_url = 'http://space-facts.com/mars/'\n",
-    "\n",
-    "# Use Panda's `read_html` to parse the url\n",
-    "mars_facts = pd.read_html(facts_url)\n",
-    "\n",
-    "# Find the mars facts DataFrame in the list of DataFrames as assign it to `mars_df`\n",
-    "mars_df = mars_facts[0]\n",
-    "\n",
-    "# Assign the columns `['Description', 'Value']`\n",
+    "mars_df = mars_facts[2]\n",
     "mars_df.columns = ['Description','Value']\n",
-    "\n",
-    "# Set the index to the `Description` column without row indexing\n",
     "mars_df.set_index('Description', inplace=True)\n",
-    "\n",
-    "# Save html code to folder Assets\n",
     "mars_df.to_html()\n",
-    "\n",
-    "data = mars_df.to_dict(orient='records')  # Here's our added param..\n",
-    "\n",
-    "# Display mars_df\n",
+    "data = mars_df.to_dict(orient='records')\n",
     "mars_df"
    ]
   },
   {
    "cell_type": "code",
-   "execution_count": null,
+   "execution_count": 13,
    "metadata": {},
    "outputs": [],
    "source": [
-    "# Visit hemispheres website through splinter module \n",
-    "hemispheres_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'\n",
+    "hemispheres_url = \"https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars\"\n",
     "browser.visit(hemispheres_url)"
    ]
   },
   {
    "cell_type": "code",
-   "execution_count": null,
+   "execution_count": 14,
    "metadata": {},
    "outputs": [],
    "source": [
-    "# HTML Object\n",
-    "html_hemispheres = browser.html\n",
-    "\n",
-    "# Parse HTML with Beautiful Soup\n",
-    "soup = BeautifulSoup(html_hemispheres, 'html.parser')\n",
-    "\n",
+    "html = browser.html\n",
+    "soup = bs(html, \"html.parser\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 16,
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "[{'title': 'Cerberus Hemisphere Enhanced',\n",
+       "  'img_url': 'https://astrogeology.usgs.gov/cache/images/f5e372a36edfa389625da6d0cc25d905_cerberus_enhanced.tif_full.jpg'},\n",
+       " {'title': 'Schiaparelli Hemisphere Enhanced',\n",
+       "  'img_url': 'https://astrogeology.usgs.gov/cache/images/3778f7b43bbbc89d6e3cfabb3613ba93_schiaparelli_enhanced.tif_full.jpg'},\n",
+       " {'title': 'Syrtis Major Hemisphere Enhanced',\n",
+       "  'img_url': 'https://astrogeology.usgs.gov/cache/images/555e6403a6ddd7ba16ddb0e471cadcf7_syrtis_major_enhanced.tif_full.jpg'},\n",
+       " {'title': 'Valles Marineris Hemisphere Enhanced',\n",
+       "  'img_url': 'https://astrogeology.usgs.gov/cache/images/b3c7c6c9138f57b4756be9b9c43e3a48_valles_marineris_enhanced.tif_full.jpg'}]"
+      ]
+     },
+     "execution_count": 16,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
     "# Retreive all items that contain mars hemispheres information\n",
     "items = soup.find_all('div', class_='item')\n",
     "\n",
     "# Create empty list for hemisphere urls \n",
     "hemisphere_image_urls = []\n",
     "\n",
-    "# Store the main_ul \n",
+    "# Store the main_url \n",
     "hemispheres_main_url = 'https://astrogeology.usgs.gov'\n",
     "\n",
     "# Loop through the items previously stored\n",
     "for i in items: \n",
+    "    \n",
     "    # Store title\n",
     "    title = i.find('h3').text\n",
     "    \n",
@@ -237,7 +318,7 @@
     "    partial_img_html = browser.html\n",
     "    \n",
     "    # Parse HTML with Beautiful Soup for every individual hemisphere information website \n",
-    "    soup = BeautifulSoup( partial_img_html, 'html.parser')\n",
+    "    soup = bs(partial_img_html, \"html.parser\")\n",
     "    \n",
     "    # Retrieve full image source \n",
     "    img_url = hemispheres_main_url + soup.find('img', class_='wide-image')['src']\n",
@@ -249,6 +330,13 @@
     "# Display hemisphere_image_urls\n",
     "hemisphere_image_urls"
    ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
   },
   {
    "cell_type": "code",
